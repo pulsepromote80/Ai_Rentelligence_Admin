1e9ae5ace289d99db9b85967836fe9af341c59a5
pulsePromote80 <pulsepromote80@gmail.com>
2025-10-28 17:04:15 +0530

    fixed responsivness

--- src/app/footer/page.js
+++ src/app/footer/page.js
@@ -3,7 +3,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="w-full py-4 text-center text-black bg-white shadow-lg">
+    <footer className="w-full py-4 h-10 text-center text-black bg-white shadow-lg">
       © Copyright {new Date().getFullYear()} | All Rights Reserved
     </footer>
   );
